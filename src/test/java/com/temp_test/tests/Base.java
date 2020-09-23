3bb3696e4af7f47b6ebd05fa6e808fabf986295c
thaleshXor <45616470+thaleshXor@users.noreply.github.com>
2020-09-23 23:35:12 +0530

    Update Base.java

--- src/test/java/com/temp_test/tests/Base.java
+++ src/test/java/com/temp_test/tests/Base.java
@@ -19,62 +19,7 @@ import org.testng.annotations.Parameters;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
-    protected WebDriver driver;
-	//String browserName=System.getProperty("BROWSER");
-    String completeURL="http://34.122.0.237:4444/wd/hub";
-    //-------------------------------------------RUN ON  Agent Node------------------------------------------
-   
-    @Parameters({"browserName"})
-    @BeforeMethod
-	public void initialSetUp1(String browserName) throws MalformedURLException {
-        if(browserName.equalsIgnoreCase("CHROME")) {
-			System.out.println(">>>>>>>>>>>>>>>>>>>>RUNNING ON CHROME<<<<<<<<<<<<<<<<<<<<");
-			WebDriverManager.chromedriver().setup();
-			DesiredCapabilities dc=new DesiredCapabilities();
-	        dc.setBrowserName("chrome");
-	        ChromeOptions options=new ChromeOptions();
-			options.addArguments("--no-sandbox");
-			options.addArguments("--headless");
-	        options.merge(dc);
-			driver=new RemoteWebDriver(new URL(completeURL), options);
-        }
-        else if(browserName.equalsIgnoreCase("FIREFOX")) {
-			System.out.println(">>>>>>>>>>>>>>>>>>>>RUNNING ON FIREFOX<<<<<<<<<<<<<<<<<<<<");
-	        WebDriverManager.firefoxdriver().setup();
-	        DesiredCapabilities dc=new DesiredCapabilities();
-	        dc.setBrowserName("firefox");
-	        FirefoxOptions options=new FirefoxOptions();
-	        options.addArguments("--no-sandbox");
-	        options.addArguments("--headless");
-	        options.merge(dc);
-	        driver=new RemoteWebDriver(new URL(completeURL), options);
+    
 
-        }
-        else {
-        	
-        	try {
-        		throw new Exception("No BROWSER NAME WAS PROVIDED !!");
-        	}
-        	catch(Exception e) {
-        		e.printStackTrace();
-        	}
-        }
-        
-        this.driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-	}
-	
-	
-	
-	@AfterMethod
-	public void tearDown(ITestResult result) {
-		
-		System.out.println("Executing : tearDown()");
-		this.driver.quit();
-		String name=result.getMethod().getMethodName();
-		System.out.println("---------Test : " + name + " ends-----------");
-		System.out.print("\n");
-		
-		
-	}
 
 }
